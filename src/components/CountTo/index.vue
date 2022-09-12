5af85a18e1d186bf8d959967f23823f5923b9620
jiahao.wu <1696764441@qq.com>
2022-09-12 18:23:03 +0800

    抽离数据文件

---
+++ src/components/CountTo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    startValue: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    endValue: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: false,
+      default: 2600,
+    },
+  },
+  data(){
+    return{
+        curValue: 0,
+    }
+  },
+  mounted() {},
+};
+</script>
